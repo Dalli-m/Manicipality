12cea1f5fabcee2c3e39ec7bfe922ffbcec0b0d8
Dalli-m <22110008@students.liu.edu.lb>
2025-08-12 12:37:21 +0300

    added new pack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,30 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
 import image1 from '../assets/image1.png'; 
-import { Link } from 'react-router-dom';
-import './Header.css';
-import image1 from '../assets/image1.png'; 
-import { useMobileMenu } from '../hooks/useMobileMenu';
 
 export default function Header() {
-  useMobileMenu();
+  
+
   return (
     <header className="municipality-header">
       <div className="header-top">
         <div className="logo">
           <img src={image1} alt="Barelias Municipality Logo" />
-          <h1>Barelias Municipality</h1>
+          <h1>Barelias Municipality</h1> {/* Only one h1 per page */}
         </div>
-        <div className="quick-links">
-
-        </div>
+        {/* Remove quick-links if unused */}
       </div>
       <nav>
         <ul>
-          <h1><Link to="/admin/login">Admin Login</Link></h1>
-          <h1><li><Link to="/">Home</Link></li></h1>
-          <h1><li><Link to="/services">Services</Link></li></h1>
-          <h1><li><Link to="/news">News</Link></li></h1>
-          <h1><li><Link to="/events">Events</Link></li></h1>
-          <h1><li><Link to="/report-issue">Report Issue</Link></li></h1>
-          <h1><li><Link to="/Contact">Contact</Link></li></h1>
-          
+          <li><Link to="/admin/login">Admin_Login</Link></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/news">News</Link></li>
+          <li><Link to="/events">Events</Link></li>
+          <li><Link to="/report-issue">Report_Issue</Link></li>
+          <li><Link to="/contact">Contact</Link></li> 
         </ul>
       </nav>
     </header>
-    
   );
 }
